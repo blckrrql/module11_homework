fb4bc4eedfec740676d20948a439c5e514bdf382
Valeriia Sitka <111923324+blckrrql@users.noreply.github.com>
2023-01-31 22:52:31 +0300

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-let num = prompt("Введите число");
-
-if ((num % 2) == 0 && !isNaN(num)) {
-  console.log(`Число ${num} чётное`);
-} else if ((num % 2) !== 0 && !isNaN(num)) {
-  console.log(`Число ${num} нечётное`);
-} else {
-    console.log("Данные неверны");
-}
